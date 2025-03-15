be5a28ac17e2756c6943abc310298a225cd70b68
yubin <59639035+gogumaC@users.noreply.github.com>
2025-03-15 14:29:30 +0900

    Time: 0 ms (100%), Space: 8.3 MB (24.23%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.c
+++ 0014-longest-common-prefix/0014-longest-common-prefix.c
@@ -1,31 +1,17 @@
 char* longestCommonPrefix(char** strs, int strsSize) {
+    if(strsSize==0){
+        return 0;
+    }
     char* common = strs[0];
-
-    int common_last = strlen(common);
-
-    for(int i = 1; i<strsSize; i++)
-    {
-        char* compare = strs[i];
-
-        int common_current =0;
-        int compare_current =0;
-
-        while(common_current<common_last && compare_current <common_last)
-        {
-            if(common[common_current]==compare[compare_current])
-            {
-                common_current++;
-                compare_current++;
+    for(int i =0; i<strsSize; i++){
+        while(*common){
+            if(strstr(strs[i],common)!=strs[i]){
+                common[strlen(common)-1]='\0';
             }else{
-                if(common_last>common_current)
-                    common_last = common_current;
                 break;
             }
-
-        }  
+        }
+        
     }
-    char* res = (char *)malloc(sizeof(char) * (common_last+1));
-    strncpy(res,common,common_last);
-    res[common_last]='\0';;
-    return res;
+    return common;
 }
