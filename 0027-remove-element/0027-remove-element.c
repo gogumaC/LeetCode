de0ff9e4fc04d67133b41d6d98a3920d6e7e1223
yubin <59639035+gogumaC@users.noreply.github.com>
2025-05-22 00:03:10 +0900

    Time: 0 ms (100%), Space: 9.7 MB (59.57%) - LeetHub

--- 0027-remove-element/0027-remove-element.c
+++ 0027-remove-element/0027-remove-element.c
@@ -1,16 +1,10 @@
-#define SWAP(a,b) do{\
-    int tmp = a;\
-    a = b;\
-    b= tmp;\ 
-}while(0)
-
 int removeElement(int* nums, int numsSize, int val) {
     int t = 0;
     int r = numsSize-1;
 
     while(t<=r){
         if(nums[t]==val){
-            SWAP(nums[t],nums[r]);
+            nums[t]=nums[r];
             r--;
         }else{
             t++;
