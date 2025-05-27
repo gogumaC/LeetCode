154d22dd468df6afb2c680c9f713a467a61e951f
yubin <59639035+gogumaC@users.noreply.github.com>
2025-05-27 22:15:33 +0900

    Time: 0 ms (100%), Space: 9.4 MB (23.6%) - LeetHub

--- 0066-plus-one/0066-plus-one.c
+++ 0066-plus-one/0066-plus-one.c
@@ -4,16 +4,16 @@
 int* plusOne(int* digits, int digitsSize, int* returnSize) {
 
     *returnSize = digitsSize;
-    int* res=digits;
     for(int i=digitsSize-1;i>=0;i--){
         printf("%d\n",i);
         if(digits[i]==9){
             digits[i]=0;
             if(i==0){
-                res = (int*)malloc(sizeof(int)*(digitsSize+1));
+                int*res = (int*)malloc(sizeof(int)*(digitsSize+1));
                 memcpy(res+1,digits,sizeof(int)*digitsSize);
                 res[0]=1;
-                free(digits);
+                free(digits--);
+                digits = res;
                 (*returnSize)++;
             }
             
@@ -24,6 +24,6 @@ int* plusOne(int* digits, int digitsSize, int* returnSize) {
     }
 
 
-    return res;
+    return digits;
     
 }
