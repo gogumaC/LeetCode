8a19c6143b0911335c4002df5345d79e3447123c
yubin <59639035+gogumaC@users.noreply.github.com>
2025-05-28 23:32:52 +0900

    Time: 0 ms (100%), Space: 8.1 MB (30.41%) - LeetHub

--- 0067-add-binary/0067-add-binary.c
+++ 0067-add-binary/0067-add-binary.c
@@ -6,17 +6,13 @@ char* addBinary(char* a, char* b) {
     uint16_t res_idx = (a_idx>b_idx?a_idx:b_idx)+1;
     char* res = (char*)malloc(res_idx+2);
 
-    //printf("a %d b %d res %d\n",a_idx,b_idx,res_idx);
     res[res_idx+1]='\0';
-        printf("size %d\n",strlen(res));
     memset(res,'0',res_idx+1);
     memcpy(res+(res_idx-a_idx-1)+1,a,a_idx+1);
     free(a);
 
-    printf("\n");
     bool add=false;
     while(res_idx>=0){
-           // printf( "res %d b %d add %d\n",res_idx,b_idx,add);
 
         int r = res[res_idx]-'0';
 
@@ -34,7 +30,6 @@ char* addBinary(char* a, char* b) {
                 add=true;
             }
         res[res_idx]='0'+r;
-        //printf("res: %c , r %d\n",res[res_idx],r);
         if(!add&& b_idx<0){
             break;
         }
@@ -43,7 +38,6 @@ char* addBinary(char* a, char* b) {
 
     }
     free(b);
-    //printf("d %d\n",res_idx);
     return res_idx>0? res+1:res;
     
 }
