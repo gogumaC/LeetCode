137d193d35996c880d9db9cba4c1c95f9b929943
yubin <59639035+gogumaC@users.noreply.github.com>
2025-06-03 00:30:43 +0900

    Time: 0 ms (100%), Space: 10 MB (59.77%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.c
+++ 0088-merge-sorted-array/0088-merge-sorted-array.c
@@ -8,11 +8,9 @@ void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
         //printf("i %d/%d i1 %d/%d i2 %d/%d\n",i,m+n,i1,m,i2,n);
         if(i1<m && (i2==n || nums1[i1]<nums2[i2]) ) {
             tmp[i]=nums1[i1];
-            //printf("1 : %d <-%d\n",i,nums1[i1]);
             i1++;
         }else if(i2<n && (i1==m || nums2[i2]<=nums1[i1])){
             tmp[i]=nums2[i2];
-            //printf("2 : %d <-%d\n",i,nums2[i2]);
             i2++;
         }
 
@@ -20,5 +18,6 @@ void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
     }
 
     memcpy(nums1,tmp,(m+n)*sizeof(int));
+    free(tmp);
     
 }
