f9f6ed1f36b93d192eb0e38f979c6c06a6bba11f
yubin <59639035+gogumaC@users.noreply.github.com>
2025-06-06 23:13:49 +0900

    Time: 0 ms (100%), Space: 8.2 MB (63.67%) - LeetHub

--- 0100-same-tree/0100-same-tree.c
+++ 0100-same-tree/0100-same-tree.c
@@ -8,8 +8,8 @@
  */
 bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
 
-    if(p == NULL && q==NULL) return true;
-    else if(p==NULL || q==NULL) return false;
-    else return (p->val == q->val) && isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
+    if(!q && !p) return true;
+    if(!p || !q || (p->val != q->val)) return false;
+    else return  isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
     
 }
