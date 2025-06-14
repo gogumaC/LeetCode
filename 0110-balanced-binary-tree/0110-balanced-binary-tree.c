afa18d61e98a8be931ed72213997de200e0c75df
yubin <59639035+gogumaC@users.noreply.github.com>
2025-06-14 17:13:51 +0900

    Time: 0 ms (100%), Space: 13.1 MB (10.82%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.c
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.c
@@ -20,7 +20,7 @@ int divideAndConquere(struct TreeNode* root, int depth, bool* res){
 
     if(root->right!=NULL) left = divideAndConquere(root->right,depth+1,&l_res);
     if(root->left!=NULL) right = divideAndConquere(root->left,depth+1,&r_res);
-    printf("-->d: %d r: %d %d l: %d %d\n",depth,r_res,right,l_res,left);
+    //printf("-->d: %d r: %d %d l: %d %d\n",depth,r_res,right,l_res,left);
     if(!r_res || !l_res ||abs(left-right)>1) *res = false;
     return MAX(left,right);
 }
@@ -31,6 +31,6 @@ bool isBalanced(struct TreeNode* root) {
     if(root==NULL) return true;
     bool res=true;
     int depth =divideAndConquere(root,0,&res);
-    printf("%d ,%d\n",res, depth);
+    //printf("%d ,%d\n",res, depth);
     return res;
 }
