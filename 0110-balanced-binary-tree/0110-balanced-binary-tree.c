b9d16f5a1aa4b21ff11219b921d332144ea2902c
yubin <59639035+gogumaC@users.noreply.github.com>
2025-06-14 17:13:33 +0900

    Time: 14 ms (4.88%), Space: 13.3 MB (10.82%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.c
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.c
@@ -10,22 +10,27 @@
 #define MAX(a,b) a>b?a:b
 
 
-int getMaxDepth(struct TreeNode* root, int depth){
+int divideAndConquere(struct TreeNode* root, int depth, bool* res){
 
     if(root->left==NULL && root->right ==NULL) return depth;
-    else if(root->left==NULL) return getMaxDepth(root->right,depth+1);
-    else if(root->right ==NULL) return getMaxDepth(root->left,depth+1);
-    
-    return MAX(getMaxDepth(root->left,depth+1),getMaxDepth(root->right,depth+1));
+    bool l_res,r_res;
+    l_res =true; r_res=true;
+    int left=depth;
+    int right =depth;
+
+    if(root->right!=NULL) left = divideAndConquere(root->right,depth+1,&l_res);
+    if(root->left!=NULL) right = divideAndConquere(root->left,depth+1,&r_res);
+    printf("-->d: %d r: %d %d l: %d %d\n",depth,r_res,right,l_res,left);
+    if(!r_res || !l_res ||abs(left-right)>1) *res = false;
+    return MAX(left,right);
 }
 
-bool isBalanced(struct TreeNode* root) {
-    if(root ==NULL) return true;
-    else if(root->left==NULL && root->right ==NULL) return true;
-    int left=0;
-    int right =0;
-    if(root->right!=NULL) right = getMaxDepth(root->right,1);
-    if(root->left!=NULL) left = getMaxDepth(root->left,1);
 
-    return isBalanced(root->left) && isBalanced(root->right) && abs(left-right)<2;
+
+bool isBalanced(struct TreeNode* root) {
+    if(root==NULL) return true;
+    bool res=true;
+    int depth =divideAndConquere(root,0,&res);
+    printf("%d ,%d\n",res, depth);
+    return res;
 }
