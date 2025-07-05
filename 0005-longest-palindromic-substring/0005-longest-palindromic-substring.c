29e35a4c19955b29b752703c06400320466e12a7
yubin <59639035+gogumaC@users.noreply.github.com>
2025-07-05 17:37:36 +0900

    Time: 1041 ms (8.52%), Space: 8.2 MB (81.94%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.c
@@ -0,0 +1,32 @@
+bool isPalindrom(char* s, int len){
+    for(int i=0;i<len/2;i++){
+        if(s[i]!=s[len-i-1]){
+            return false;
+        }
+    }
+    return true;
+}
+
+char* longestPalindrome(char* s) {
+
+    int max = 0;
+    int len = strlen(s);
+    int start = 0,end=0;
+
+    for(int i=0;i<len;i++){
+        for(int j=i;j<len;j++){
+            int size = j-i+1;
+            if(isPalindrom(s+i,size)){
+                //printf("i:%d j:%d s:%d\n",i,j,size);
+                if(max<size){
+                    max=size;
+                    start=i; end=j;
+                }
+            }
+        }
+    }
+
+    *(s+end+1) = '\0';
+
+    return s+start;    
+}
