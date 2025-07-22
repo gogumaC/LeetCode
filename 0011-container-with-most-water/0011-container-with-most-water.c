eb8006847231dfeeeb19e0b6ed29d4646de8a21e
yubin <59639035+gogumaC@users.noreply.github.com>
2025-07-22 21:38:45 +0900

    Time: 0 ms (100%), Space: 13.9 MB (97.99%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.c
+++ 0011-container-with-most-water/0011-container-with-most-water.c
@@ -1,14 +1,21 @@
+#define MAX(a,b) ((a>b)?a:b)
+#define MIN(a,b) ((a<b)?a:b)
+
 int maxArea(int* height, int heightSize) {
+    int left=0;
+    int right = heightSize-1;
     int max=0;
-    for(int x1=0;x1<heightSize;x1++){
-        for(int x2=heightSize-1;x2>x1;x2--){
-            int w= x2-x1;
-            if(w*height[x1]<max) break;
-            int h = (height[x1]>height[x2])?height[x2]:height[x1];
-            int area = h*w;
-           // printf("%d %d - %d\n",x1,x2,area);
-            max = (area>max)?area:max;
+    while(left<right){
+        int h = MIN(height[left],height[right]);
+        int area = h*(right-left);
+
+        max = MAX(max, area);
+        if(height[left]<height[right]){
+            left++;
+        }else{
+            right--;
         }
+
     }
 
     return max;
