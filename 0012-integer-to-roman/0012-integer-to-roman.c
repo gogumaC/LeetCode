c18f998c1778a81d958ef79f7c7b0349885657c8
yubin <59639035+gogumaC@users.noreply.github.com>
2025-07-26 21:09:35 +0900

    Time: 0 ms (100%), Space: 8.4 MB (75.12%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.c
+++ 0012-integer-to-roman/0012-integer-to-roman.c
@@ -1,9 +1,9 @@
-char res[17];
 int div_n[] = {1,4,5,9,10,40,50,90,100,400,500,900,1000};
 char* roman[] = {"I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"};
 
 char* intToRoman(int num) {
 
+    char* res=malloc(sizeof(char)*17);
     memset(res,0,sizeof(res));
 
     for(int i=12;i>=0;i--){
