bce8919119b6a669a3b371d9ad8c773621f4e00c
yubin <59639035+gogumaC@users.noreply.github.com>
2025-07-26 21:07:03 +0900

    Time: 0 ms (100%), Space: 8 MB (96.75%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.c
@@ -0,0 +1,18 @@
+char res[17];
+int div_n[] = {1,4,5,9,10,40,50,90,100,400,500,900,1000};
+char* roman[] = {"I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"};
+
+char* intToRoman(int num) {
+
+    memset(res,0,sizeof(res));
+
+    for(int i=12;i>=0;i--){
+        int count = num/div_n[i];
+        num%= div_n[i];
+        while(count--){
+            strcat(res,roman[i]);
+        }
+    }
+    return res;
+    
+}
